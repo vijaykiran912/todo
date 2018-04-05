9d94be2e52fc4d626252997d1ad0e10807fce69c
vipothamse-copart <Vijay.Pothamsetty@copart.com>
2018-04-05 22:08:12 +0530

    Initial changes & before running npm run eject

---
+++ src/component/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Login.css';
+
+const login = (props) => {
+
+    return (
+        <div className="classes.Login">
+            <h1>Hi..</h1>
+        </div>
+    );
+}
+
+export default login;
